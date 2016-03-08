26c60aba6275c35cb24bef7c4ec081ba6b6bf4b9
François Jacquet <francoisjacquet@users.noreply.github.com>
2016-03-08 18:49:34 -0500

    More efficient GetCurrentMessagingUser() function: use User( 'NAME' )

--- Messaging/includes/Common.fnc.php
+++ Messaging/includes/Common.fnc.php
@@ -12,32 +12,11 @@ function GetCurrentMessagingUser()
 
 	if ( ! $user )
 	{
-		$user_id = ! empty( $_SESSION['STUDENT_ID'] ) ? $_SESSION['STUDENT_ID'] : User( 'STAFF_ID' );
+		$user_id = User( 'STAFF_ID' ) ? User( 'STAFF_ID' ) : UserStudentID();
 
-		$key = ! empty( $_SESSION['STUDENT_ID'] ) ? 'student_id' : 'staff_id';
+		$key = User( 'STAFF_ID' ) ? 'staff_id' : 'student_id';
 
-		if ( $key === 'student_id' )
-		{
-			$name_RET = DBGet( DBQuery(
-				"SELECT FIRST_NAME||' '||LAST_NAME||coalesce(' '||NAME_SUFFIX,' ') AS NAME
-				FROM STUDENTS
-				WHERE STUDENT_ID='" . $user_id . "'" ) );
-		}
-		else
-		{
-			$name_RET = DBGet( DBQuery( "SELECT FIRST_NAME||' '||LAST_NAME AS NAME
-				FROM STAFF
-				WHERE STAFF_ID='" . $user_id . "'" ) );
-		}
-
-		$name = '';
-
-		if ( $name_RET
-			&& isset( $name_RET[1]['NAME'] ) )
-		{
-			// For example:Student Student.
-			$name = $name_RET[1]['NAME'];
-		}
+		$name = User( 'NAME' );
 
 		$user = array( 'user_id' => $user_id, 'key' => $key, 'name' => $name );
 	}
