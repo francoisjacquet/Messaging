444b960e7bcb8e4307993e9a96f798d58e589552
François Jacquet <francoisjacquet@users.noreply.github.com>
2017-02-02 16:00:32 -0500

    Add Module title to menu

--- Menu.php
+++ Menu.php
@@ -7,20 +7,16 @@
  * @package Messaging module
  */
 
-/**
- * Use dgettext() function instead of _() for Module specific strings translation
- * see locale/MessagesME file for more information.
- */
-$module_name = dgettext( 'Messaging', 'Messaging' );
-
 // Menu entries for the Messaging module.
 $menu['Messaging']['admin'] = array( // Admin menu.
+	'title' => dgettext( 'Messaging', 'Messaging' ),
 	'default' => 'Messaging/Messages.php', // Program loaded by default when menu opened.
 	'Messaging/Messages.php' => dgettext( 'Messaging', 'Messages' ),
 	'Messaging/Write.php' => dgettext( 'Messaging', 'Write' ),
 );
 
 $menu['Messaging']['teacher'] = array( // Teacher menu
+	'title' => dgettext( 'Messaging', 'Messaging' ),
 	'default' => 'Messaging/Messages.php', // Program loaded by default when menu opened.
 	'Messaging/Messages.php' => dgettext( 'Messaging', 'Messages' ),
 	'Messaging/Write.php' => dgettext( 'Messaging', 'Write' ),
